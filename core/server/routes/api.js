39e5c34662c837544cca9b209e029f3eee9e5df6
Harry Wolff <hswolff@gmail.com>
2014-04-11 23:46:15 -0400

    Upgrade to Express 4.0
    no related issue
    
    - Updates package.json packages, adding express middleware packages
     that have been broken into their own modules
    
    - Updates controllers/frontend.js to use the new Layer object that Express 4.0
     has.  Requires some monkey-patching as the Layer object isn't explicitly
     surfaced, however it should be safe to do.
    
    - Moved the setup of routes into middleware/index.js because they need to
     be added as a middleware function before the 404 and 500 handlers. This is
     no longer possible with the old app.use(app.router) as that has been removed.
    
    - Cleaned up middleware/index.js to make it compatible with Express 4.0.
    
    - Simplified the way themes are activated and enabled when they are activated.
     The new handling is simpler, yet should still cover all the use cases that
     previously existed.
    
    - The entire flow of activating a theme through middleware should be a little
     more centralized, letting it be easier to read and maintain.
    
    - Moved every routes/*.js file to use an individual express.Router() instance.

--- core/server/routes/api.js
+++ core/server/routes/api.js
@@ -1,33 +1,37 @@
-var middleware  = require('../middleware').middleware,
-    api         = require('../api');
+var api         = require('../api'),
+    express     = require('express');
 
-module.exports = function (server) {
+module.exports = function (middleware) {
+    var router = express.Router();
+
     // ### API routes
     // #### Posts
-    server.get('/ghost/api/v0.1/posts', api.requestHandler(api.posts.browse));
-    server.post('/ghost/api/v0.1/posts', api.requestHandler(api.posts.add));
-    server.get('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.read));
-    server.put('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.edit));
-    server.del('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.destroy));
-    server.get('/ghost/api/v0.1/posts/slug/:title', middleware.authAPI, api.requestHandler(api.posts.generateSlug));
+    router.get('/ghost/api/v0.1/posts', api.requestHandler(api.posts.browse));
+    router.post('/ghost/api/v0.1/posts', api.requestHandler(api.posts.add));
+    router.get('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.read));
+    router.put('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.edit));
+    router['delete']('/ghost/api/v0.1/posts/:id', api.requestHandler(api.posts.destroy));
+    router.get('/ghost/api/v0.1/posts/slug/:title', middleware.authAPI, api.requestHandler(api.posts.generateSlug));
     // #### Settings
-    server.get('/ghost/api/v0.1/settings/', api.requestHandler(api.settings.browse));
-    server.get('/ghost/api/v0.1/settings/:key/', api.requestHandler(api.settings.read));
-    server.put('/ghost/api/v0.1/settings/', api.requestHandler(api.settings.edit));
+    router.get('/ghost/api/v0.1/settings/', api.requestHandler(api.settings.browse));
+    router.get('/ghost/api/v0.1/settings/:key/', api.requestHandler(api.settings.read));
+    router.put('/ghost/api/v0.1/settings/', api.requestHandler(api.settings.edit));
     // #### Users
-    server.get('/ghost/api/v0.1/users/', api.requestHandler(api.users.browse));
-    server.get('/ghost/api/v0.1/users/:id/', api.requestHandler(api.users.read));
-    server.put('/ghost/api/v0.1/users/:id/', api.requestHandler(api.users.edit));
+    router.get('/ghost/api/v0.1/users/', api.requestHandler(api.users.browse));
+    router.get('/ghost/api/v0.1/users/:id/', api.requestHandler(api.users.read));
+    router.put('/ghost/api/v0.1/users/:id/', api.requestHandler(api.users.edit));
     // #### Tags
-    server.get('/ghost/api/v0.1/tags/', api.requestHandler(api.tags.browse));
+    router.get('/ghost/api/v0.1/tags/', api.requestHandler(api.tags.browse));
     // #### Notifications
-    server.del('/ghost/api/v0.1/notifications/:id', api.requestHandler(api.notifications.destroy));
-    server.post('/ghost/api/v0.1/notifications/', api.requestHandler(api.notifications.add));
+    router['delete']('/ghost/api/v0.1/notifications/:id', api.requestHandler(api.notifications.destroy));
+    router.post('/ghost/api/v0.1/notifications/', api.requestHandler(api.notifications.add));
     // #### Import/Export
-    server.get('/ghost/api/v0.1/db/', api.requestHandler(api.db.exportContent));
-    server.post('/ghost/api/v0.1/db/', middleware.busboy, api.requestHandler(api.db.importContent));
-    server.del('/ghost/api/v0.1/db/', api.requestHandler(api.db.deleteAllContent));
+    router.get('/ghost/api/v0.1/db/', api.requestHandler(api.db.exportContent));
+    router.post('/ghost/api/v0.1/db/', middleware.busboy, api.requestHandler(api.db.importContent));
+    router['delete']('/ghost/api/v0.1/db/', api.requestHandler(api.db.deleteAllContent));
     // #### Mail
-    server.post('/ghost/api/v0.1/mail', api.requestHandler(api.mail.send));
-    server.post('/ghost/api/v0.1/mail/test', api.requestHandler(api.mail.sendTest));
+    router.post('/ghost/api/v0.1/mail', api.requestHandler(api.mail.send));
+    router.post('/ghost/api/v0.1/mail/test', api.requestHandler(api.mail.sendTest));
+
+    return router;
 };
