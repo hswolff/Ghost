9a5adf28f758de08ad7fc9574723594e32468996
lennerd <hello@lennerd.com>
2014-05-05 17:00:18 +0200

    Fix permalink route to change, when settings have changed.

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -1,14 +1,14 @@
 /*global require, module */
 
 var frontend = require('../controllers/frontend'),
-    Router = require('./router'),
+    ghostRouter = require('./router'),
     api = require('../api'),
 
     ONE_HOUR_S = 60 * 60,
     ONE_YEAR_S = 365 * 24 * ONE_HOUR_S;
 
 module.exports = function () {
-    var router = new Router();
+    var router = ghostRouter();
 
     // ### Redirect '/feed' to '/rss'
     router.get('feed', '/feed/', function redirect(req, res, next) {
@@ -18,7 +18,7 @@ module.exports = function () {
 
     // ### Multiple posts routes
     router.get('browse_tags', '/tag/:tag/:page?/', frontend.tag);
-    router.get('browse', '/:page?', frontend.homepage);
+    router.get('browse', '/:page?/', frontend.homepage);
 
     // ### RSS routes
     router.get('rss_tags_page', '/rss/:tag/page/:page/', frontend.rss);
@@ -26,17 +26,23 @@ module.exports = function () {
     router.get('rss_page', '/rss/page/:page/', frontend.rss);
     router.get('rss', '/rss/', frontend.rss);
 
-    // ### Add permalink route
-    api.settings.read('permalinks').then(function (result) {
-        var permalink = result.settings[0].value,
-            editFormat = permalink[permalink.length - 1] === '/' ? ':edit?' : '/:edit?';
+    // ### Add permalink route in extra middleware
+    // We need this middleware for keeping a up to date permalink route, because the permalink settings can change after
+    // the router has been initialized.
+    router.use(function (req, res, next) {
+        var permalinkRouter = ghostRouter();
 
-        // Add the permalink to the frontend router, so we can check it next
-        router.get('permalink', permalink + editFormat, frontend.single);
-    }).then(function () {
-        // ### Route for a static post
-        router.get('static', '/:slug/:edit?', frontend.single);
+        api.settings.read('permalinks').then(function (result) {
+            var permalink = result.settings[0].value,
+                editFormat = permalink[permalink.length - 1] === '/' ? ':edit?' : '/:edit?';
+
+            permalinkRouter.get('permalink', permalink + editFormat, frontend.single);
+            permalinkRouter(req, res, next);
+        });
     });
+
+    // ### Route for a static post
+    router.get('static', '/:slug/:edit?', frontend.single);
 
     return router;
 };
